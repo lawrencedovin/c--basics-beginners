430f0f1baa4ff5da2f9c9290944c00ff8844574f
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-22 10:51:12 -0500

    Added dates examples

---
+++ section-7-dates/Dates/Dates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            // Getting Information
+            //Console.WriteLine("Hour: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
+
+            // Get tomorrow or yesterday at the same exact time
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            // Outputting DateTime
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm tt"));
+        }
+    }
+}
