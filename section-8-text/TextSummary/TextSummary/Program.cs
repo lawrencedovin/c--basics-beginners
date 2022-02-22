180ce625ffa3e4c3cdb795c8052a0a3e546e4cb9
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-22 13:26:40 -0500

    Added summarize text

---
+++ section-8-text/TextSummary/TextSummary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSummary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentences = @"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+sapiente officiis modi at sunt excepturi expedita sint?";
+
+            const int MaxLength = 40;
+
+            var summary = Summary.summarizeText(sentences, MaxLength);
+            Console.WriteLine(summary); 
+        }
+    }
+}
---
+++ section-8-text/TextSummary/TextSummary/Summary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSummary
+{
+    internal class Summary
+    {
+        public static string summarizeText(string sentences, int MaxLength = 20) {
+            if (sentences.Length < MaxLength)
+            {
+                return sentences;
+            }
+            else
+            {
+                var totalCharacters = 0;
+                var words = sentences.Split(' ');
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > MaxLength)
+                        break;
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                return summary;
+            }
+        }
+    }
+}
