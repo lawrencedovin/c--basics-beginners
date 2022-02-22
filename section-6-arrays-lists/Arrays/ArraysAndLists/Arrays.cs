a828f1d661b92d09e4547aecdaead0b528037146
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-22 09:45:56 -0500

    Added array examples

---
+++ section-6-arrays-lists/Arrays/ArraysAndLists/Arrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    internal class Arrays
+    {
+        public static void displayNumArray(int[] numbers)
+        {
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ section-6-arrays-lists/Arrays/ArraysAndLists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array
+            var numbers = new int[] { 3, 6, 7, 2, 14 };
+            //Arrays.displayNumArray(numbers);
+
+            // Length
+            Console.WriteLine(String.Format("Length: {0}", numbers.Length));
+
+            // IndexOf()
+            int index = Array.IndexOf(numbers, 2);
+            Console.WriteLine(String.Format("Index of {0}: {1}", 2, index));
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+            Console.WriteLine("Effect of Clear()");
+            foreach (int n in numbers)
+                Console.WriteLine(n);
+
+            // Copy
+            var numbers2 = new int[3];
+            Array.Copy(numbers, numbers2, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (int n in numbers2)
+                Console.WriteLine(n);
+
+            // Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Copy()");
+            foreach(int n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse()
+            Array.Reverse(numbers);
+            foreach(int n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
