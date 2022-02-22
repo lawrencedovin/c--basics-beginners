aa5a4b1d5e565fe3fdd6833fdd18e36086c396de
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-22 14:31:03 -0500

    Added Demo Directory and Directory info example

--- section-9-files/DirectoryDemo/DirectoryDemo/Program.cs
+++ section-9-files/DirectoryDemo/DirectoryDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace DirectoryDemo
     {
         static void Main(string[] args)
         {
+            // Directory
+            Directory.CreateDirectory(@"C:\Users\lawre\Desktop\Coding\c#\c#-basics-beginners\section-8-text\new");
+            // Gets all the files ending with .sln
+            var files = Directory.GetFiles(@"C:\Users\lawre\Desktop\Coding\c#\c#-basics-beginners\section-8-text", "*.sln", SearchOption.AllDirectories);
+            foreach(var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"C:\Users\lawre\Desktop\Coding\c#\c#-basics-beginners\section-8-text", "*", SearchOption.AllDirectories);
+            foreach(var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            // Directory Info
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
         }
     }
 }
