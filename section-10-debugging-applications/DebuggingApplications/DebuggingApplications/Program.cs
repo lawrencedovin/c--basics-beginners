d784e352d8d0d208cdd7a2c1794a428fc0394877
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-22 15:51:15 -0500

    Added debugging example

---
+++ section-10-debugging-applications/DebuggingApplications/DebuggingApplications/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingApplications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
